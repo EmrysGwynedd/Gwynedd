bbab9f5c2f33208659ac8324f5d6a3886c4d2e20
EmrysGwynedd <emryswynevans@gwynedd.llyw.cymru>
2025-06-05 09:27:20 +0100

    Create Lenovo_CHECK.ps1

---
+++ Lenovo_Update/Lenovo_CHECK.ps1
@@ -0,0 +1,14 @@
+# Powershell sy'n uwchraddio BIOS ac Firmware unrhyw ddyfais Lenovo
+
+# Angen System Account Cred i allu gweld SCCM-Rheolwr-01
+$cred = Get-Credential
+New-PSDrive -Name V -PSProvider FileSystem -Root \\sccm-rheolwr-01\d$\Lenovo_Driver_Repository -Persist -Credential $cred
+
+# Gosod y module
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+Install-Module -Name 'LSUClient'
+
+# Hel gwybodaeth am y ddyfais
+gcim Win32_ComputerSystem | fl Manufacturer, Model, SystemFamily
+Get-LSUpdate -Repository \\sccm-rheolwr-01\d$\Lenovo_Driver_Repository
+Get-LSUpdate | Tee-Object -Variable updates
