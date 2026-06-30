71cf155e69d607af3c78626be9254551c2633b8f
emrys <emryswynevans@gwynedd.llyw.cymru>
2026-06-30 16:04:39 +0100

    Add PowerShell scripts for BIOS Secure Boot configuration

--- SANDBOX/Lenovo/BIOS_SecureBoot/BIOS_SecureBoot_Set.ps1
+++ SANDBOX/Lenovo/BIOS_SecureBoot/BIOS_SecureBoot_Set.ps1
@@ -12,8 +12,8 @@ SecureBoot,Enable
 "@ | Out-File "C:\TG\BIOS_SecureBoot\Settings_CSV_SecureBoot.csv" -Encoding UTF8 -Force
 
 # Copi o Manage-LenovoBiosSettings.ps1
-Copy-Item "\\me-dtcentral\DCSWRepository\Lenovo\BIOS_SecureBoot\Manage-LenovoBiosSettings.ps1" "C:\TG\BIOS_SecureBoot" -Force
+Invoke-WebRequest "https://raw.githubusercontent.com/ConfigJon/Firmware-Management/master/Lenovo/Manage-LenovoBiosSettings.ps1" -OutFile "C:\TG\BIOS_SecureBoot\Manage-LenovoBiosSettings.ps1" -UseBasicParsing
 
 # Rhedeg y script i newid settings bios yn y ffeil csv
-C:\TG\BIOS\Manage-LenovoBiosSettings.ps1 -SetSettings -CsvPath C:\TG\BIOS\Settings_CSV_SecureBoot.csv -SupervisorPassword cg679114cg
-C:\TG\BIOS\Manage-LenovoBiosSettings.ps1 -SetSettings -CsvPath C:\TG\BIOS\Settings_CSV_SecureBoot.csv -SupervisorPassword milamber1
+C:\TG\BIOS_SecureBoot\Manage-LenovoBiosSettings.ps1 -SetSettings -CsvPath C:\TG\BIOS_SecureBoot\Settings_CSV_SecureBoot.csv -SupervisorPassword cg679114cg
+C:\TG\BIOS_SecureBoot\Manage-LenovoBiosSettings.ps1 -SetSettings -CsvPath C:\TG\BIOS_SecureBoot\Settings_CSV_SecureBoot.csv -SupervisorPassword milamber1
