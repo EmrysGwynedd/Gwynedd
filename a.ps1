bada9a0694ce6aaa38f6d9dc7ac950a9527579f7
emrys <emryswynevans@gwynedd.llyw.cymru>
2026-09-08 13:22:43 +0100

    remove initial setup script a.ps1

--- a.ps1
+++
@@ -1,9 +0,0 @@
-Remove-Item .git -Recurse -Force
-
-git init
-git branch -M main
-git add .
-git commit -m "Initial commit"
-
-git remote add origin https://github.com/EmrysGwynedd/Gwynedd.git
-git push -u origin main --force
